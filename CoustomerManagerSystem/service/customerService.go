7a21f7f8ca8fab330150238edab5b98c6c8b9de6
Sirius <wxr_624@163.com>
2022-03-05 23:51:46 +0800

    学习mvc第二天

--- CoustomerManagerSystem/service/customerService.go
+++ CoustomerManagerSystem/service/customerService.go
@@ -1,6 +1,8 @@
 package service
 
-import "mytest/CoustomerManagerSystem/model"
+import (
+	"mytest/CoustomerManagerSystem/model"
+)
 
 // CustomerService 该CustomerService ,完成对Customer的操作
 //增删改查
@@ -9,4 +11,17 @@ type CustomerService struct {
 	//声明一个字段，表示当前切片有多少客户
 	//该字段后，还可以作为新客户的id+1
 	customerNum int
+}
+
+func NewCustomerService() *CustomerService {
+	CustomerService := &CustomerService{}
+	CustomerService.customerNum = 1
+	var customer model.Customer = model.NewCustomer(1, "Siri", "男", 20, "13391110017", "siri@qq.com")
+	CustomerService.customers = append(CustomerService.customers, customer)
+	return CustomerService
+
+}
+
+func (c *CustomerService) List() []model.Customer {
+	return c.customers
 }
--- CoustomerManagerSystem/view/customerView.go
+++ CoustomerManagerSystem/view/customerView.go
@@ -1,15 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"mytest/CoustomerManagerSystem/service"
+)
 
 type customerView struct {
 	//定义必要字段
 	key string
 	//表示是否循环显示菜单
 	loop bool
+
+	customerService *service.CustomerService
 }
 
-func (cv *customerView) mainMenu() {
+func (this *customerView) list() {
+	customers := this.customerService.List()
+	fmt.Println("----------客户列表----------")
+	fmt.Println("编号\t姓名\t性别\t年龄\t电话\t\t邮箱")
+	for i := 0; i < len(customers); i++ {
+		fmt.Println(customers[i].GetInfo())
+	}
+	fmt.Println("---------客户列表完成---------")
+}
+
+func (this *customerView) mainMenu() {
 	for {
 		fmt.Println("----------客户信息管理软件----------")
 		fmt.Println("          1 添加客户")
@@ -19,8 +34,8 @@ func (cv *customerView) mainMenu() {
 		fmt.Println("          5 退出")
 		fmt.Print("请选择（1-5）：")
 
-		fmt.Scanln(&cv.key)
-		switch cv.key {
+		fmt.Scanln(&this.key)
+		switch this.key {
 		case "1":
 			fmt.Println("添加客户")
 		case "2":
@@ -28,14 +43,15 @@ func (cv *customerView) mainMenu() {
 		case "3":
 			fmt.Println("删除客户")
 		case "4":
-			fmt.Println("客户列表")
+			//list函数是此对象的函数，所以要用this，不能使用结构体名
+			this.list()
 		case "5":
 			fmt.Println("退出")
-			cv.loop = false
+			this.loop = false
 		default:
 			fmt.Println("输入有误，请重新输入...")
 		}
-		if !cv.loop {
+		if !this.loop {
 			fmt.Println("你退出了客户关系管理系统")
 			break
 		}
@@ -44,7 +60,8 @@ func (cv *customerView) mainMenu() {
 }
 
 func main() {
-	//在主函数中创建一个CustonerView并运行显示主菜单..
+	//在主函数中创建一个CustomerView并运行显示主菜单..
 	customerView := customerView{key: "", loop: true}
+	customerView.customerService = service.NewCustomerService()
 	customerView.mainMenu()
 }
