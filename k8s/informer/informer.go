06eee2e3f391c47b8a1c0cf8198eea1f69ebd0a3
Wang <xirui.wang@philips.com>
2022-11-22 19:35:25 +0800

    update

---
+++ k8s/informer/informer.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/sirupsen/logrus"
+	v1 "k8s.io/api/apps/v1"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/client-go/informers"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/cache"
+	klog "k8s.io/klog/v2"
+	"sigs.k8s.io/controller-runtime/pkg/client/config"
+)
+
+func main() {
+	conf, err := config.GetConfig()
+
+	if err != nil {
+		logrus.Panic(err)
+	}
+	clientSet, err := kubernetes.NewForConfig(conf)
+	informerFactory := informers.NewSharedInformerFactory(clientSet, 30*time.Second)
+	deployInformer := informerFactory.Apps().V1().Deployments()
+	informer := deployInformer.Informer()
+	deployListener := deployInformer.Lister()
+	// 资源事件
+	informer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc:    onAdd,
+		UpdateFunc: onUpdate,
+		DeleteFunc: onDelete,
+	})
+	stopper := make(chan struct{})
+	defer close(stopper)
+	// 启动informer List & Watch
+	informerFactory.Start(stopper)
+	// 等待所有缓存同步
+	informerFactory.WaitForCacheSync(stopper)
+	deployments, err := deployListener.Deployments("default").List(labels.Everything())
+	for index, deploy := range deployments {
+		fmt.Printf("%d -> %s", index, deploy.Name)
+
+	}
+	<-stopper
+}
+func onAdd(obj interface{}) {
+	deploy := obj.(*v1.Deployment)
+	klog.Infoln("add a deploy: ", deploy.Name)
+}
+
+func onUpdate(old, new interface{}) {
+	oldDeploy := old.(*v1.Deployment)
+	newDeploy := new.(*v1.Deployment)
+	klog.Infoln("update deploy: ", oldDeploy.Status.Replicas, newDeploy.Status.Replicas)
+}
+
+func onDelete(obj interface{}) {
+	deploy := obj.(*v1.Deployment)
+	klog.Infoln("delete a deploy:", deploy.Name)
+}
